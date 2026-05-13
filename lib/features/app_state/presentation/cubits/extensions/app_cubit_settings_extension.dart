0085cc1a84b03638e436036d346b189a57c5ec5e
albasuny9 <albasuny9@gmail.com>
2026-05-13 13:17:42 +0300

    refactor: introduce settings cubit extension foundation

---
+++ lib/features/app_state/presentation/cubits/extensions/app_cubit_settings_extension.dart
@@ -0,0 +1,6 @@
+import '../app_cubit.dart';
+
+extension AppCubitSettingsExtension on AppCubit {
+  // Settings and preferences mutations will gradually move here.
+  // This reduces unrelated responsibilities from the core AppCubit.
+}
