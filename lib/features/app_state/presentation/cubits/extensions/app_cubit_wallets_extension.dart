45bd86bb5a6c2b0fc38c34fc485012935373402e
albasuny9 <albasuny9@gmail.com>
2026-05-13 13:16:25 +0300

    refactor: introduce wallet cubit extension foundation

---
+++ lib/features/app_state/presentation/cubits/extensions/app_cubit_wallets_extension.dart
@@ -0,0 +1,7 @@
+import '../app_cubit.dart';
+
+extension AppCubitWalletsExtension on AppCubit {
+  // Wallet-related mutations will gradually move here.
+  // This extension is the first step toward splitting
+  // AppCubit by domain responsibilities.
+}
