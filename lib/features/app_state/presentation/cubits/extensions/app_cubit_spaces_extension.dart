b7ccc9c149978bfb33f9c559e748b6d9e78ab839
albasuny9 <albasuny9@gmail.com>
2026-05-13 13:16:49 +0300

    refactor: introduce spaces cubit extension foundation

---
+++ lib/features/app_state/presentation/cubits/extensions/app_cubit_spaces_extension.dart
@@ -0,0 +1,8 @@
+import '../app_cubit.dart';
+
+extension AppCubitSpacesExtension on AppCubit {
+  // Savings jars / financial spaces mutations
+  // will gradually move here.
+  // This prepares future migration from jars
+  // to financial spaces architecture.
+}
