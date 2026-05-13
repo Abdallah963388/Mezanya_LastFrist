d6875db70e0617065f1e23acffd4844287298a0e
albasuny9 <albasuny9@gmail.com>
2026-05-13 13:16:38 +0300

    refactor: introduce transaction cubit extension foundation

---
+++ lib/features/app_state/presentation/cubits/extensions/app_cubit_transactions_extension.dart
@@ -0,0 +1,7 @@
+import '../app_cubit.dart';
+
+extension AppCubitTransactionsExtension on AppCubit {
+  // Transaction-related mutations will gradually move here.
+  // This extension prepares AppCubit decomposition
+  // without breaking existing runtime behavior.
+}
