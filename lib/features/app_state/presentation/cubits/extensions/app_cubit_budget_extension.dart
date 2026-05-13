21f5d54ebaf61d8a185534a2a8c347736a5c413f
albasuny9 <albasuny9@gmail.com>
2026-05-13 13:17:30 +0300

    refactor: introduce budget cubit extension foundation

---
+++ lib/features/app_state/presentation/cubits/extensions/app_cubit_budget_extension.dart
@@ -0,0 +1,7 @@
+import '../app_cubit.dart';
+
+extension AppCubitBudgetExtension on AppCubit {
+  // Budget and allocation mutations will gradually move here.
+  // This keeps financial budgeting responsibilities isolated
+  // from wallet and transaction orchestration.
+}
